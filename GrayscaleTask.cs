2f3549512f39c7cfb6f556e9f65200de5d6206f1
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-08-19 03:23:57 +0300

    Добавьте файлы проекта.

---
+++ GrayscaleTask.cs
@@ -0,0 +1,35 @@
+﻿namespace Recognizer
+{
+	public static class GrayscaleTask
+	{
+		/* 
+		 * Переведите изображение в серую гамму.
+		 * 
+		 * original[x, y] - массив пикселей с координатами x, y. 
+		 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
+		 * 
+		 * Получившийся массив должен иметь те же размеры, 
+		 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
+		 *
+		 * Используйте формулу:
+		 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
+		 * 
+		 * Почему формула именно такая — читайте в википедии 
+		 * http://ru.wikipedia.org/wiki/Оттенки_серого
+		 */
+
+		public static double[,] ToGrayscale(Pixel[,] original)
+		{
+			var jober = new double[original.GetLength(0), original.GetLength(1)];
+			int length = original.GetLength(0);
+			int widht = original.GetLength(1);
+			for (int i = 0; i < length; i++)
+				for (int j = 0; j < widht; j++)
+                {
+					jober[i, j] = (0.299 * original[i, j].R + 0.587 * original[i, j].G + 0.114 * original[i, j].B) 
+						/ 255;
+                }
+			return jober;
+		}
+	}
+}

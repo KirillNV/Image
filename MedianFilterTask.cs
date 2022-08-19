a759c115427a3eeb45f721d61b88d9d117d8bd3b
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-08-19 05:10:39 +0300

    Add metods in class MedianFilterTask for filter midle element

--- MedianFilterTask.cs
+++ MedianFilterTask.cs
@@ -15,7 +15,53 @@ namespace Recognizer
 		 */
 		public static double[,] MedianFilter(double[,] original)
 		{
-			return original;
+			var jober = new double[original.GetLength(0), original.GetLength(1)];
+			int length = original.GetLength(0);
+			int widht = original.GetLength(1);
+
+			for (int i = 1; i < length - 1; i++)
+				for (int j = 1; j < widht - 1; j++)
+				{
+					var array = new double[] { original[i - 1, j - 1], original[i - 1, j], original[i - 1, j + 1], original[i, j - 1], original[i, j], original[i, j + 1], original[i + 1, j - 1], original[i + 1, j], original[i + 1, j + 1]};
+					jober[i, j] = SortMedian(array);
+				}
+			return jober;
+		}
+
+		public static double SortMedian(double[] pixelMedian)
+		{
+			
+			if(pixelMedian.Length % 2 == 0)
+            {
+				var median = SortBubble(pixelMedian);
+				int leftElement = (int)((median.Length / 2) - 0.4);
+				int rightElement = (int)((median.Length / 2) - 0.4);
+				return (median[leftElement] + median[rightElement]) / 2;
+			}
+            else 
+			{
+				var median = SortBubble(pixelMedian);
+				int midleElement = (int)((median.Length / 2) - 0.1);
+				return median[midleElement];
+			}	
+		}
+
+		static double[] SortBubble(double[] myArray)
+		{
+			double temp;
+			for (int i = 0; i < myArray.Length; i++)// перебор "1-ого" элемента
+			{
+				for (int j = i + 1; j < myArray.Length; j++)// перебор "2-ого" элемента
+				{
+					if (myArray[i] > myArray[j])//сравние близике элементы
+					{                           // первый элемент больше второго, то меняем местами
+						temp = myArray[i];
+						myArray[i] = myArray[j];
+						myArray[j] = temp;
+					}
+				}
+			}
+			return myArray;
 		}
 	}
 }

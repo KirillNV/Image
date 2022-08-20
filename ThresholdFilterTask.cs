a6414d2e8855eaad872b18b0832230a441cf29e0
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-08-20 05:24:20 +0300

    class ThresholdFilterTask work but can't to used with tests.

--- ThresholdFilterTask.cs
+++ ThresholdFilterTask.cs
@@ -1,10 +1,32 @@
-﻿namespace Recognizer
+﻿using System.Collections.Generic;
+
+namespace Recognizer
 {
 	public static class ThresholdFilterTask
 	{
 		public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
 		{
-			return new double[original.GetLength(0), original.GetLength(1)];
+			int length = original.GetLength(0);
+			int widht = original.GetLength(1);
+			var pixelList = new List<double>();
+			int T = (int)(length * widht * whitePixelsFraction);
+
+			for(int i = 0; i < length; i++)
+				for (int j = 0; j < widht; j++)
+					pixelList.Add(original[i,j]);
+			pixelList.Sort();
+
+			double maxPixel = pixelList[pixelList.Count - T];
+			var finishPixel = new double[original.GetLength(0), original.GetLength(1)];
+			for (int i = 0; i < length; i++)
+				for (int j = 0; j < widht; j++)
+                {
+					if (original[i, j] < maxPixel)
+						finishPixel[i, j] = 0.0;
+					else finishPixel[i, j] = 1.0;
+				}
+
+			return finishPixel;
 		}
 	}
 }

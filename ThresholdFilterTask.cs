a6aa8c81311d76fa4e9348710c1a34a668227cbf
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-08-23 01:09:55 +0300

    Finished ThresholdFilterTask

--- ThresholdFilterTask.cs
+++ ThresholdFilterTask.cs
@@ -8,25 +8,40 @@ namespace Recognizer
 		{
 			int length = original.GetLength(0);
 			int widht = original.GetLength(1);
-			var pixelList = new List<double>();
+
 			int T = (int)(length * widht * whitePixelsFraction);
+			double maxPixel = FiltredPixel(CreateSortedList(original, length, widht), T);
 
-			for(int i = 0; i < length; i++)
-				for (int j = 0; j < widht; j++)
-					pixelList.Add(original[i,j]);
-			pixelList.Sort();
-
-			double maxPixel = pixelList[pixelList.Count - T];
 			var finishPixel = new double[original.GetLength(0), original.GetLength(1)];
 			for (int i = 0; i < length; i++)
 				for (int j = 0; j < widht; j++)
-                {
-					if (original[i, j] < maxPixel)
-						finishPixel[i, j] = 0.0;
-					else finishPixel[i, j] = 1.0;
+				{
+					if (original[i, j] >= maxPixel)
+						finishPixel[i, j] = 1.0;
+					else finishPixel[i, j] = 0.0;
 				}
 
 			return finishPixel;
+		}
+
+		public static List<double> CreateSortedList(double[,] original, int length, int widht)
+		{
+			var pixelList = new List<double>();
+			for (int i = 0; i < length; i++)
+				for (int j = 0; j < widht; j++)
+					pixelList.Add(original[i, j]);
+			pixelList.Sort();
+			return pixelList;
+		}
+
+		public static double FiltredPixel(List<double> pixelList, int T)
+		{
+			double maxPixel;
+			if (T == 0)
+				maxPixel = double.MaxValue;
+			else
+				maxPixel = pixelList[pixelList.Count - T];
+			return maxPixel;
 		}
 	}
 }
